e4154425091b772c729dc2b01f1df6519c47705a
Dino Priyano <dinopriyano>
2021-02-19 00:39:43 +0700

    adding insert and update karyawan

--- src/java/dao/KaryawanDao.java
+++ src/java/dao/KaryawanDao.java
@@ -5,10 +5,120 @@
  */
 package dao;
 
+import connection.CliniclyConnection;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.KaryawanModel;
+import model.PasienModel;
+import static utils.Encryption.encryptSHA256;
+
 /**
  *
  * @author dinop
  */
 public class KaryawanDao {
+    private final Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public KaryawanDao() {
+        this.conn = CliniclyConnection.connection();
+    }
     
+    public String getNewIdKaryawan(){
+        String newID = null;
+        try{
+            String lastID = null;
+            String query="{CALL GetLastIdKaryawan()}";
+            ps=conn.prepareCall(query);
+            rs=ps.executeQuery();
+            if(rs.next()){
+                lastID = rs.getString("id_karyawan");
+                String lastNumStr = lastID.substring(2);
+                int newNumInt = Integer.parseInt(lastNumStr)+1;
+                String newNumStr = "000"+newNumInt;
+                String fixNewNumStr = newNumStr.substring(newNumStr.length()-4);
+                newID = "KR"+fixNewNumStr;
+            }
+            else{
+                newID = "KR0001";
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+        return newID;
+        
+    }
+    
+    public void save(KaryawanModel model,String page){
+        try{
+            String query = null;
+            String doneMessage = null;
+            if(page.equals("insert")){
+                query="{CALL InsertKaryawan(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+                doneMessage = "Insert success!";
+            }
+            else if(page.equals("update")){
+                query="{CALL UpdateKaryawan(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+                doneMessage = "Update success!";
+            }
+            
+            ps=conn.prepareCall(query);
+            ps.setString(1, model.getEmail());
+            ps.setString(2, model.getId_role());
+            ps.setString(3, model.getUser_id());
+            ps.setString(4, model.getPassword());
+            ps.setBoolean(5, model.isStatus());
+            ps.setString(6, model.getId_karyawan());
+            ps.setString(7, model.getNama_karyawan());
+            ps.setDate(8, model.getTgl_lahir());
+            ps.setString(9, model.getBidang_pekerjaan());
+            ps.setString(10, model.getJenis_kelamin());
+            ps.setString(11, model.getNo_ktp());
+            ps.setString(12, model.getNo_npwp());
+            ps.setString(13, model.getAlamat());
+            ps.setString(14, model.getNo_hp());
+            ps.setString(15, model.getCreated_user_id());
+            ps.setDate(16, model.getWaktu());
+            ps.executeUpdate();
+            System.out.println(doneMessage);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        KaryawanDao dao = new KaryawanDao();
+        PasienDao pasienDao = new PasienDao();
+//        System.out.println(dao.getNewIdKaryawan());
+        long millis=System.currentTimeMillis();  
+        Date date = new Date(millis);
+       
+        String newUserID = pasienDao.getNewIdUser();
+        String newKaryawanID = dao.getNewIdKaryawan();
+        
+        KaryawanModel model = new KaryawanModel();
+        model.setEmail("marpuah@email.com");
+        model.setId_role("R3");
+        model.setUser_id("US0006");
+        model.setPassword(encryptSHA256("pass"));
+        model.setStatus(true);
+//        model.setId_karyawan(newKaryawanID);
+        model.setNama_karyawan("Marpuah");
+        model.setTgl_lahir(date);
+        model.setBidang_pekerjaan("Apoteker");
+        model.setJenis_kelamin("P");
+        model.setNo_ktp("28034020");
+        model.setNo_npwp("248399984");
+        model.setAlamat("Namex");
+        model.setNo_hp("4938023923");
+//        model.setWaktu(date);
+//        model.setCreated_user_id("US0001");
+        dao.save(model, "update");
+    }
 }
--- src/java/model/KaryawanModel.java
+++ src/java/model/KaryawanModel.java
@@ -5,10 +5,152 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author dinop
  */
 public class KaryawanModel {
+    String id_karyawan,nama_karyawan,bidang_pekerjaan,jenis_kelamin,alamat,no_hp,no_ktp,no_npwp,user_id,created_user_id,email,password,id_role,des_role;
+    Date tgl_lahir,waktu;
+    boolean status;
+
+    public String getId_karyawan() {
+        return id_karyawan;
+    }
+
+    public void setId_karyawan(String id_karyawan) {
+        this.id_karyawan = id_karyawan;
+    }
+
+    public String getNama_karyawan() {
+        return nama_karyawan;
+    }
+
+    public void setNama_karyawan(String nama_karyawan) {
+        this.nama_karyawan = nama_karyawan;
+    }
+
+    public String getBidang_pekerjaan() {
+        return bidang_pekerjaan;
+    }
+
+    public void setBidang_pekerjaan(String bidang_pekerjaan) {
+        this.bidang_pekerjaan = bidang_pekerjaan;
+    }
+
+    public String getJenis_kelamin() {
+        return jenis_kelamin;
+    }
+
+    public void setJenis_kelamin(String jenis_kelamin) {
+        this.jenis_kelamin = jenis_kelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNo_hp() {
+        return no_hp;
+    }
+
+    public void setNo_hp(String no_hp) {
+        this.no_hp = no_hp;
+    }
+
+    public String getNo_ktp() {
+        return no_ktp;
+    }
+
+    public void setNo_ktp(String no_ktp) {
+        this.no_ktp = no_ktp;
+    }
+
+    public String getNo_npwp() {
+        return no_npwp;
+    }
+
+    public void setNo_npwp(String no_npwp) {
+        this.no_npwp = no_npwp;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCreated_user_id() {
+        return created_user_id;
+    }
+
+    public void setCreated_user_id(String created_user_id) {
+        this.created_user_id = created_user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId_role() {
+        return id_role;
+    }
+
+    public void setId_role(String id_role) {
+        this.id_role = id_role;
+    }
+
+    public String getDes_role() {
+        return des_role;
+    }
+
+    public void setDes_role(String des_role) {
+        this.des_role = des_role;
+    }
+
+    public Date getTgl_lahir() {
+        return tgl_lahir;
+    }
+
+    public void setTgl_lahir(Date tgl_lahir) {
+        this.tgl_lahir = tgl_lahir;
+    }
+
+    public Date getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(Date waktu) {
+        this.waktu = waktu;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
     
 }
